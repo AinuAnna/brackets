e7bc59564c26829e598e15c33f00c6bd9eb04a06
AinuAnna <65246069+AinuAnna@users.noreply.github.com>
2020-10-23 10:31:32 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let Newbrackets = new Map(bracketsConfig),
+   stack = [];
+
+   for (let i = 0; i< str.length; i++)
+   {
+     str[i] === Newbrackets.get(stack[stack.length-1]) ? stack.pop() : stack.push(str[i]);
+   }
+   return stack.length === 0;
 }
